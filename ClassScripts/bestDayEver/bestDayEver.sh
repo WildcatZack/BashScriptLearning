0b284a666f670f0d2db4601a3c987cf5ccbf4096
Zachary Farrow <farrow.zachary@gmail.com>
2023-04-27 18:17:07 +0000

    finished bestDayEver

--- ClassScripts/bestDayEver/bestDayEver.sh
+++ ClassScripts/bestDayEver/bestDayEver.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-echo 'Good Morning Zachary!'
+
+# Getting a variable using Read
+#
+#echo "What is your name?"
+#
+# read name
+
+
+# Getting a variable using a positional argument:
+name=$1
+compliment=$2
+
+
+# Get variable from bash command
+userid=$(whoami)
+whereami=$(pwd)
+date=$(date)
+
+echo "You are currently logged in as $userid and you are in the directory $whereami. Also today is: $date"
+
+echo "Good Morning $name!"
 
 sleep 1
 
-echo "You're looking good today!"
+echo "You're looking good today $name!"
 
 sleep 1
 
-echo "You have the best beard I've ever seen Zachary!"
+echo "You have the best $compliment I've ever seen $name!"
